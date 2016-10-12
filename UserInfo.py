a2e33b5f82135743760017cd79b51794354ff6cf
atlantis-maya <zhuhe88@live.cn>
2016-10-12 23:14:07 +0800

    添加用户模块,增加了一些运行状态的信息

---
+++ UserInfo.py
@@ -0,0 +1,27 @@
+#!/bin/bash/env python3
+
+
+class user(object):
+	"""关于linux系统下用户信息的查询,各种操作"""
+	def __init__(self):
+		pass
+
+	def get_usernum(self):
+		"""获取用户数量"""
+		pass
+
+	def get_username(self):
+		"""获取用户名"""
+		pass
+	def add_user(self):
+		"""增加用户"""
+		pass
+	def del_user(self):
+		"""删除用户"""
+		pass
+	def add_group():
+		"""添加用户组"""
+		pass
+	def del_group(self):
+		"""删除用户组"""
+		pass
